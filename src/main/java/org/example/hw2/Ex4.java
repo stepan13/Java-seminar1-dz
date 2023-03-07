69e2eda2ae5ecbbd43846c523ec994bc843dd04f
Vanger <stepan13@gmail.com>
2023-03-07 19:53:57 +0000

    второй метод решения судоку

--- src/main/java/org/example/hw2/Ex4.java
+++ src/main/java/org/example/hw2/Ex4.java
@@ -17,11 +17,11 @@ public class Ex4 {
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
         };
 
-        System.out.println("Validete:" + isValidSudoku(board));
+        System.out.println("Validate1:" + isValidSudoku(board));
+        System.out.println("Validate2:" + isValidSudoku2(board));
     }
 
     /**
-     *
      * @param board двумерный массив судоку
      * @return истина если в судоку нет ошибок
      */
@@ -29,36 +29,64 @@ public class Ex4 {
         for (int i = 0; i < board.length; i++) {
             if (!rowCorrect(board[i])) return false;
         }
-        for (int i = 0; i < board[0].length; i++){
+        for (int i = 0; i < board[0].length; i++) {
             if (!columnCorrect(board, i)) {
                 return false;
             }
         }
         for (int i = 0; i < board.length; i += 3) {
             for (int j = 0; j < board[0].length; j += 3) {
-                if (!blockCorrect(board,i,j)) return false;
+                if (!blockCorrect(board, i, j)) return false;
             }
         }
         return true;
     }
 
     /**
-     *
+     * @apiNote второй вариант решения с одной дополнительной функцией
+     * @param board доска судоку
+     * @return истина если в судоку нет ошибок
+     */
+    private static boolean isValidSudoku2(char[][] board) {
+        char[] row = new char[board.length];
+        char[] col = new char[board.length];
+        for (int i = 0; i < board.length; i++) {
+            row = board[i].clone();
+            if (!rowCorrect(row)) return false;
+            for (int j = 0; j < board.length; j++) {
+                col[j] = board[j][i];
+            }
+            if (!rowCorrect(col)) return false;
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        row[3 * k + l] = board[3 * i + k][3 * j + l];
+                    }
+                }
+                if (!rowCorrect(row)) return false;
+            }
+        }
+        return true;
+
+    }
+
+    /**
      * @param chars массив символов ряда
      * @return истина если в ряду нет повторений
      */
     private static boolean rowCorrect(char[] chars) {
         String allstring = "";
         for (char ch : chars) {
-            if (stringContains(ch,allstring)) return false;
+            if (stringContains(ch, allstring)) return false;
             allstring += ch;
         }
         return true;
     }
 
     /**
-     *
-     * @param board вся доска судоку
+     * @param board        вся доска судоку
      * @param columnNumber номер проверяемой колонки
      * @return истина если в колонке нет повторений
      */
@@ -67,15 +95,14 @@ public class Ex4 {
         char ch;
         for (int i = 0; i < board[0].length; i++) {
             ch = board[i][columnNumber];
-            if (stringContains(ch,allstring)) return false;
+            if (stringContains(ch, allstring)) return false;
             allstring += ch;
         }
         return true;
     }
 
     /**
-     *
-     * @param board вся доска судоке
+     * @param board    вся доска судоке
      * @param startRow индекс первой строки блока
      * @param StartCol индекс первой колонки блока
      * @return истина если в блоке нет повторений
@@ -85,8 +112,8 @@ public class Ex4 {
         char ch;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                ch = board[startRow+i][StartCol+j];
-                if (stringContains(ch,allstring)) return false;
+                ch = board[startRow + i][StartCol + j];
+                if (stringContains(ch, allstring)) return false;
                 allstring += ch;
             }
         }
@@ -94,15 +121,21 @@ public class Ex4 {
     }
 
     /**
-     *
-     * @param ch искомый символ
+     * @param ch     искомый символ
      * @param string строка в которой ищем
      * @return входит ли символ в данную строку
      */
-    public static boolean stringContains(char ch, String string){
+    public static boolean stringContains(char ch, String string) {
         if (ch != '.') {
-            if (string.contains(Character.toString(ch))) return true;
+            return string.contains(Character.toString(ch));
         }
         return false;
+    }
+    private static void printArray(char[] array) {
+        String res = "[";
+        for (int i = 0; i < array.length; i++) {
+            res += array[i] + (i == array.length - 1 ? "]" : ",");
+        }
+        System.out.println(res);
     }
 }
